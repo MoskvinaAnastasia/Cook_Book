f56bc4ec6742dd12f7ae3fe2c47c4ebb31dbc7ba
Анастасия Москвина <A.E.Moskvina@yandex.ru>
2024-08-18 19:45:03 +0300

    Updated infra and workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -19,6 +19,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
