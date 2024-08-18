ef08593a8c6e64ff4fb3e95d9d9c83df5cae8e15
Анастасия Москвина <A.E.Moskvina@yandex.ru>
2024-08-18 18:27:29 +0300

    new10

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -20,8 +20,8 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
-
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
