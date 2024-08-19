e805e18105a5fd4564df37251b68121311dd2944
Анастасия Москвина <A.E.Moskvina@yandex.ru>
2024-08-19 22:10:37 +0300

    1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -19,6 +19,10 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(';')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
